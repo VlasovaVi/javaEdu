33241e2fb9ac27e673c84d6b15b21f5a2ae35354
Рахманкулов Эдуард <Rakhmankulov.E.R@sberbank.ru>
2020-04-21 20:43:56 +0300

    тесты на partialSort

--- src/test/java/ru/victoria/arrays/ArrayPartialSortTest.java
+++ src/test/java/ru/victoria/arrays/ArrayPartialSortTest.java
@@ -6,10 +6,44 @@ import org.testng.annotations.Test;
 public class ArrayPartialSortTest {
 
     @Test
-    public void testFindMaxIdx(){
+    public void testFindMaxIdx1(){
         int[] num1 = {1,2,3,4};
         int result = ArrayPartialSort.findMaxIdx(num1);
 
         Assert.assertEquals(result, 3);
     }
+
+    @Test
+    public void testFindMaxIdx2(){
+        int[] num1 = {3,52,1,5,8,12};
+        int result = ArrayPartialSort.findMaxIdx(num1);
+
+        Assert.assertEquals(result, 1);
+    }
+
+    @Test
+    public void testFindMaxIdx3(){
+        int[] num1 = {};
+        int result = ArrayPartialSort.findMaxIdx(num1);
+
+        Assert.assertEquals(result, -1);
+    }
+
+    @Test
+    public void testFindMinSecondIdx() {
+        int[] arg = {3,52,1,5,8,12};
+        int result = ArrayPartialSort.findMinSecondIdx(arg);
+
+        Assert.assertEquals(result, arg.length - 1);
+
+        arg = new int[]{};
+        result = ArrayPartialSort.findMinSecondIdx(arg);
+        Assert.assertEquals(result, -1);
+
+
+        arg = new int[]{1};
+        result = ArrayPartialSort.findMinSecondIdx(arg);
+        Assert.assertEquals(result, -1);
+    }
+
 }
