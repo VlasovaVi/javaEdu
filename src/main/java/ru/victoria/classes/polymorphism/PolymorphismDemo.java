3710a05ee773a0574d63dce682b37d0ab9140710
VlasovaVi <vlasova.v.o8936@gmail.com>
2020-06-20 01:51:10 +0300

    Polymorphism

--- src/main/java/ru/victoria/classes/polymorphism/PolymorphismDemo.java
+++ src/main/java/ru/victoria/classes/polymorphism/PolymorphismDemo.java
@@ -10,11 +10,66 @@ public class PolymorphismDemo {
     }
 
     /**
+     *
      * if lhs > rhs => 1
      * if lhs == rhs => 0
      * if lhs < rhs => -1
      */
     public static int compareFigures(Figure lhs, Figure rhs) {
+            if (lhs instanceof Circle && rhs instanceof Circle){
+                Circle cirL = (Circle) lhs;
+                Circle cirR = (Circle) rhs;
+                if(cirL.getPerimeter() > cirR.getPerimeter() ) {
+                    return 1;
+                }
+                if(cirL.getPerimeter() == cirR.getPerimeter() ){
+                    return 0;
+                }
+                if(cirL.getPerimeter() < cirR.getPerimeter()){
+                    return -1;
+                }
+        }
+        if (lhs instanceof Triangle && rhs instanceof Triangle){
+            Triangle triangleL = (Triangle) lhs;
+            Triangle triangleR = (Triangle) rhs;
+            if(triangleL.getArea() > triangleR.getArea()){
+                return 1;
+            }
+            if(triangleL.getArea() == triangleR.getArea()){
+                return 0;
+            }
+            if(triangleL.getArea() < triangleR.getArea()){
+                return -1;
+            }
+
+        }
+        if (lhs instanceof Rectangle && rhs instanceof Rectangle){
+
+            Rectangle rectangleL = (Rectangle) lhs;
+            Rectangle rectangleR = (Rectangle) rhs;
+            if(rectangleL.getPerimeter() > rectangleR.getPerimeter()){
+                return 1;
+            }
+            if(rectangleL.getPerimeter() == rectangleR.getPerimeter()){
+                return 0;
+            }
+            if(rectangleL.getPerimeter() < rectangleR.getPerimeter()){
+                return -1;
+            }
+        }
+        if(lhs instanceof Rectangle && rhs instanceof Triangle){
+            Rectangle rectangleL = (Rectangle) lhs;
+            Triangle triangleR = (Triangle) rhs;
+            if(rectangleL.getPerimeter() > triangleR.getPerimeter()){
+                return 1;
+            }
+            if(rectangleL.getPerimeter() == triangleR.getPerimeter()){
+                return 0;
+            }
+            if(rectangleL.getPerimeter() < triangleR.getPerimeter()){
+                return -1;
+            }
+        }
         return 0;
     }
 
